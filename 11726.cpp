a1334c9463b8a91db0796cc721d36e15c4b03a35
tlstk1101 <tlstk1101@gmail.com>
2019-02-13 10:05:45 +0900

    2xn 타일링

---
+++ 11726.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int array_d[1001];
+
+int dp(int x) {
+	if (x == 1) 
+		return 1;
+	if (x == 2) 
+		return 2;
+	if (array_d[x] != 0)
+		return array_d[x];
+	return array_d[x] = (dp(x - 1) + dp(x - 2)) % 10007;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	
+	cout << dp(n) << endl;
+	return 0;
+}
