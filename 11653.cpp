c71851a395e10c59083b85e835138260501cade4
tlstk1101 <tlstk1101@gmail.com>
2019-08-16 14:11:01 +0900

    소인수분해

---
+++ 11653.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	while (N > 1) {
+		for (int i = 2; i <= N; i++) {
+			if (N % i == 0) {				
+				cout << i << endl;
+				N /= i;
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
